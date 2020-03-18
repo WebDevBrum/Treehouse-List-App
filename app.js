b34c2b0fd2de9fbc5466acfd10d372345d428131
Scott Davies <57508310+theDevelopingDeveloper@users.noreply.github.com>
2020-03-18 07:42:45 +0000

    Up/down/remove now created with javascript only
    Functionality added so new list items include buttons

--- app.js
+++ app.js
@@ -6,8 +6,30 @@ const descriptionButton = document.querySelector('button.description');
 const listUl = listDiv.querySelector('ul'); // notice how query selector was used more locally
 const addItemInput = document.querySelector('input.addItemInput');
 const addItemButton = document.querySelector('button.addItemButton');
+const lis = listUl.children;
 // const removeItemButton = document.querySelector('button.removeItemButton');
 
+function attachListItemButtons(li) {
+    let up = document.createElement('button')
+    up.className = 'up';
+    up.textContent = 'Up';
+    li.appendChild(up);
+
+    let down = document.createElement('button')
+    down.className = 'down';
+    down.textContent = 'Down';
+    li.appendChild(down);
+
+    let remove = document.createElement('button')
+    remove.className = 'remove';
+    remove.textContent = 'Remove';
+    li.appendChild(remove);
+}
+
+// attaches buttons to items on load
+for (let i = 0; i < lis.length; i += 1) {
+    attachListItemButtons(lis[i]);
+}
 
 listUl.addEventListener('click', (event) => { // was mouseover for uppercase hover , AND LISTENER WAS ORIGINALLY listDiv
   if(event.target.tagName == 'BUTTON'){// WAS LI
@@ -61,10 +83,12 @@ descriptionButton.addEventListener('click', () => {
   descriptionInput.value = '';                                 
 });
 
+//also attaches up/down/remove buttons
 addItemButton.addEventListener('click', () => {
     let ul = document.getElementsByTagName('ul')[0];
     let li = document.createElement('li'); 
     li.textContent = addItemInput.value;
+    attachListItemButtons(li);
     ul.appendChild(li);
     addItemInput.value = '';
 });
